f18577bc7dd84655515305a0a1af17a0f17ca550
jayef7 <filhojanio67@gmail.com>
2022-04-19 20:00:05 -0300

    Projeto

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Cassio Ramos',
+                partido: 'Corinthians',
+                fotos: [
+                    {url: '289390.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Neymar Junior',
+                partido: 'PSG',
+                fotos: [
+                    {url: '563129.png', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Nathalia',
+                partido: 'Amor',
+                vice: 'Janio',
+                fotos: [
+                    {url: '13.png', legenda: 'Prefeito' },
+                    {url: '13_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Benzema',
+                partido: 'RDM',
+                vice: 'Zuluzinho',
+                fotos: [
+                    {url: '19.png', legenda: 'Prefeito'},
+                    {url: '19_2.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
